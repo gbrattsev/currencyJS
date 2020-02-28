58e05b0c94d824fbe8091c2d5af03a5c2390f48b
Gennady Brattsev <56601654+gbrattsev@users.noreply.github.com>
2020-02-28 16:06:58 +0500

    set promise

--- main.js
+++ main.js
@@ -4,21 +4,37 @@ let inputRub = document.querySelector('#rub'),
     inputUsd = document.querySelector('#usd'),
     inputEuro = document.querySelector('#eur');
 
-inputRub.addEventListener('input', () => {
-  let request = new XMLHttpRequest();
-  request.open('GET', 'current.json');
-  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-  request.send();
 
-  request.addEventListener('readystatechange', function() {
-    if (request.readyState === 4 && request.status == 200) {
-      let data = JSON.parse(request.response);
-      console.log(data);
-      inputUsd.value = (inputRub.value / data.Valute.USD.Value).toFixed(2);
-      inputEuro.value = (inputRub.value / data.Valute.EUR.Value).toFixed(2);
-    } else {
-      inputUsd.value = 'Сервер не отвечает';
-      
-    }
-  });
+inputRub.addEventListener('input', () => {
+
+  function getData() {
+
+    return new Promise(function(resolve, reject) {
+
+      let request = new XMLHttpRequest();
+      request.open('GET', 'js/current.json');
+      request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+      request.send();
+
+      request.onload = function() {
+        if(request.readyState === 4) {
+                if(request.status == 200) {
+                    resolve(this.response)
+                }
+                else {
+                    reject();
+                
+                }
+        }
+      }
+    });
+  }
+  
+  getData()
+      .then(response => {
+        let data = JSON.parse(response);
+        inputUsd.value = (inputRub.value / data.Valute.USD.Value).toFixed(2);
+        inputEuro.value = (inputRub.value / data.Valute.EUR.Value).toFixed(2);
+      })
+      .catch(() => inputUsd.value = 'Сервер не отвечает')
 });
